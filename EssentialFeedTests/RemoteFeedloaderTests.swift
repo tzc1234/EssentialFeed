38c47a23c35e257da6d5676c5064cea653de7f98
tzc1234 <peopli9997@gmail.com>
2022-11-27 11:36:11 +0000

    Test requestURL is not nil when called `load`

--- EssentialFeedTests/RemoteFeedloaderTests.swift
+++ EssentialFeedTests/RemoteFeedloaderTests.swift
@@ -8,20 +8,45 @@
 import XCTest
 
 class RemoteFeedLoader {
+    private let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://any-URL.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    var requestedURL: URL? { get }
+    func get(from url: URL)
+}
+
+class HTTPClientSPY: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedloaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSPY()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
 
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSPY()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
