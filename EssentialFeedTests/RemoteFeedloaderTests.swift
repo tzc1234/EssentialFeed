137504391923a390e453cd68ff7e16ed6795139a
tzc1234 <peopli9997@gmail.com>
2022-11-27 16:34:02 +0000

    Deliver invalidData error on 200 HTTP response with invalid json

--- EssentialFeedTests/RemoteFeedloaderTests.swift
+++ EssentialFeedTests/RemoteFeedloaderTests.swift
@@ -61,6 +61,18 @@ final class RemoteFeedloaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
 }
 
 // MARK: - Helpers
@@ -89,9 +101,9 @@ extension RemoteFeedloaderTests {
             message[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = .init(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            message[index].completion(.success(response))
+            message[index].completion(.success(data, response))
         }
     }
 }
