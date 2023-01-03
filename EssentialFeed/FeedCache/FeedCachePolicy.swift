e6782f98766dbdefacbe799846f2a0b581e131a5
Tsz Lung <lung@storelab.app>
2023-01-03 08:52:21 +0000

    Move `FeedCachePolicy` to its own file

---
+++ EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Tsz Lung on 03/01/2023.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int { 7 }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
