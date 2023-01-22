31c9cc02a0d38948c909a56832da642b3483fb8d
Tsz Lung <lung@storelab.app>
2023-01-22 18:17:11 +0000

    Add EssentialFeedCacheIntegrationTests target to separate the poteltially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Tsz Lung on 22/01/2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+
+}
