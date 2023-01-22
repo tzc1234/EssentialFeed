45070954ee7762a59180ccfc43af03b2427b3efc
Tsz Lung <lung@storelab.app>
2023-01-22 11:20:59 +0000

    Extract reusable CoreData helpers into a separate file

---
+++ EssentialFeed/FeedCache/Infrastructure/CoreDataHelpers.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataHelpers.swift
+//  EssentialFeed
+//
+//  Created by Tsz Lung on 22/01/2023.
+//
+
+import CoreData
+
+extension NSPersistentContainer {
+    enum LoadingError: Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Error)
+    }
+    
+    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let description = NSPersistentStoreDescription(url: url)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+        var loadError: Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+    
+}
+
+extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        bundle.url(forResource: name, withExtension: "momd").flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
