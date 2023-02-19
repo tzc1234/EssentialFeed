094b0ec7d51c82c9ffbf6cf7121c595eff81e3a2
tzc1234 <peopli9997@gmail.com>
2023-02-19 20:38:02 +0000

    Replace occurrences of Error? for representing operation success/failure with `Result<Void, Error>`

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -117,8 +117,8 @@ extension FeedStoreSpecs where Self: XCTestCase {
     func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: FeedStore) -> Error? {
         let exp = expectation(description: "Wait for cache retrieval")
         var insertionError: Error?
-        sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
-            insertionError = receivedInsertionError
+        sut.insert(cache.feed, timestamp: cache.timestamp) { result in
+            if case let .failure(error) = result { insertionError = error }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
@@ -129,8 +129,8 @@ extension FeedStoreSpecs where Self: XCTestCase {
     func deleteCache(from sut: FeedStore) -> Error? {
         let exp = expectation(description: "Wait for deletion")
         var deleteError: Error?
-        sut.deleteCachedFeed { receivedDeleteError in
-            deleteError = receivedDeleteError
+        sut.deleteCachedFeed { result in
+            if case let .failure(error) = result { deleteError = error }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 3)
