afac8f1f2e626c54611da5828efaa9c2907eec20
Tsz Lung <lung@storelab.app>
2023-03-08 10:57:46 +0000

    Extract FeedViewController test helpers to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -365,64 +365,6 @@ final class FeedViewControllerTests: XCTestCase {
     
 }
 
-private extension FeedViewController {
-    func simulateUserInitiatedFeedReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    @discardableResult
-    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-        feedImageView(at: index) as? FeedImageCell
-    }
-    
-    func simulateFeedImageViewWillVisible(on cell: UITableViewCell, at index: Int) {
-        let delegate = tableView.delegate
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        delegate?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
-    }
-    
-    @discardableResult
-    func simulateFeedImageViewNotVisible(at index: Int) -> FeedImageCell? {
-        let view = simulateFeedImageViewVisible(at: index)
-        
-        let delegate = tableView.delegate
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: indexPath)
-        
-        return view
-    }
-    
-    func simulateFeedImageViewNearVisible(at index: Int) {
-        let ds = tableView.prefetchDataSource
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        ds?.tableView(tableView, prefetchRowsAt: [indexPath])
-    }
-    
-    func simulateFeedImageViewNotNearVisible(at index: Int) {
-        simulateFeedImageViewNearVisible(at: index)
-        
-        let ds = tableView.prefetchDataSource
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
-    }
-    
-    var isShowingLoadingIndicator: Bool {
-        refreshControl?.isRefreshing == true
-    }
-    
-    func numberOfRenderedFeedImageView() -> Int {
-        tableView.numberOfRows(inSection: feedImagesSection)
-    }
-    
-    func feedImageView(at index: Int = 0) -> UITableViewCell? {
-        let ds = tableView.dataSource
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        return ds?.tableView(tableView, cellForRowAt: indexPath)
-    }
-    
-    private var feedImagesSection: Int { 0 }
-}
-
 private extension FeedImageCell {
     func simulateRetryAction() {
         feedImageRetryButton.simulateTap()
---
+++ EssentialFeediOSTests/Helpers/FeedViewContoller+TestHelpers.swift
@@ -0,0 +1,67 @@
+//
+//  FeedViewContoller+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Tsz Lung on 08/03/2023.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedViewController {
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+        feedImageView(at: index) as? FeedImageCell
+    }
+    
+    func simulateFeedImageViewWillVisible(on cell: UITableViewCell, at index: Int) {
+        let delegate = tableView.delegate
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        delegate?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewNotVisible(at index: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewVisible(at: index)
+        
+        let delegate = tableView.delegate
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: indexPath)
+        
+        return view
+    }
+    
+    func simulateFeedImageViewNearVisible(at index: Int) {
+        let ds = tableView.prefetchDataSource
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [indexPath])
+    }
+    
+    func simulateFeedImageViewNotNearVisible(at index: Int) {
+        simulateFeedImageViewNearVisible(at: index)
+        
+        let ds = tableView.prefetchDataSource
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        refreshControl?.isRefreshing == true
+    }
+    
+    func numberOfRenderedFeedImageView() -> Int {
+        tableView.numberOfRows(inSection: feedImagesSection)
+    }
+    
+    func feedImageView(at index: Int = 0) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        return ds?.tableView(tableView, cellForRowAt: indexPath)
+    }
+    
+    private var feedImagesSection: Int { 0 }
+}
