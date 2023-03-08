830fb84a6b84c7d1737c470aeb41e4ab00812f2f
Tsz Lung <lung@storelab.app>
2023-03-08 10:32:44 +0000

    Organise the feed iOS files

---
+++ EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Tsz Lung on 08/03/2023.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
