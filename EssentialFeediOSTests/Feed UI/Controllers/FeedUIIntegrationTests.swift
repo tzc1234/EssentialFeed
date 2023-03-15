cde5fcfeeebc4d9c664749ed5e97d0321a4ba26d
Tsz Lung <lung@storelab.app>
2023-03-15 21:25:21 +0000

    Dispatch background feed image data completion to main thread before passing result to the UI components. Threading is dealt with a Decorator in the Composition layer

--- EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
@@ -307,6 +307,21 @@ final class FeedUIIntegrationTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1)
     }
+    
+    func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [makeImage()], at: 0)
+        sut.simulateFeedImageViewVisible(at: 0)
+        
+        let exp = expectation(description: "Wait for backgorund queue")
+        DispatchQueue.global().async {
+            loader.completeImageLoading(with: self.anyImageData(), at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
 
     // MARK: - Helpers
     
