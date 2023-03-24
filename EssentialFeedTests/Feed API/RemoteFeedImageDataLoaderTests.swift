950014535b5df72d26058c7c184727406afdab87
tzc1234 <peopli9997@gmail.com>
2023-03-24 19:08:39 +0000

    `RemoteFeedImageDataLoader` requests data from URL on `loadImageDataFromURL`

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -6,9 +6,18 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class RemoteFeedImageDataLoader {
-    init(client: Any) {}
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
+    }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
@@ -19,6 +28,17 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs.count, 0)
     }
     
+    func test_loadImageDataFromURL_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT()
+        
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
@@ -27,8 +47,12 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
 
-    private class HTTPClientSpy {
+    private class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            requestedURLs.append(url)
+        }
     }
     
 }
