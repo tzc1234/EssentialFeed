7e49de2c14a746cb29a4c67cfb8e08c1621475e2
tzc1234 <peopli9997@gmail.com>
2023-05-25 15:10:53 +0100

    Move test helpers to shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -65,15 +65,4 @@ extension FeedItemsMapperTests {
         
         return (item, json)
     }
-    
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
 }
