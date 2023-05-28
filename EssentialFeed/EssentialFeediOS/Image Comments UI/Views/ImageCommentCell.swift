1e464ef414062f922d15d4413813b2be95a0b1c7
tzc1234 <peopli9997@gmail.com>
2023-05-28 19:05:16 +0100

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Tsz-Lung on 28/05/2023.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import EssentialFeed
 
 public protocol CellController {
-    func view(in: UITableView) -> UITableViewCell
+    func view(in tableView: UITableView) -> UITableViewCell
     func preload()
     func startLoad()
     func cancelLoad()
