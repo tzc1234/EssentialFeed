ef365207731aa9f7a086c9e8886467c9b827b8be
tzc1234 <peopli9997@gmail.com>
2023-05-30 16:03:30 +0100

    Set comments title

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -18,7 +18,7 @@ public final class CommentsUIComposer {
     public static func commentsComposedWith(commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) -> ListViewController {
         let presentationAdapter = FeedPresentationAdapter(loader: commentsLoader)
         
-        let feedController = makeFeedViewController(title: FeedPresenter.title, onRefresh: presentationAdapter.loadResource)
+        let feedController = makeFeedViewController(title: ImageCommentsPresenter.title, onRefresh: presentationAdapter.loadResource)
         
         presentationAdapter.presenter = LoadResourcePresenter(
             resourceView: FeedViewAdapter(
