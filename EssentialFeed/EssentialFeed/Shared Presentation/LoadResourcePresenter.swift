367a84481f8d05c31875bbfffbae04141f617311
tzc1234 <peopli9997@gmail.com>
2023-06-05 15:06:44 +0100

    Add default presentation mapper when the Resource type matches the ResourceViewModel type

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -35,6 +35,13 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
         self.errorView = errorView
         self.mapper = mapper
     }
+    
+    public init(resourceView: View, loadingView: ResourceLoadingView, errorView: ResourceErrorView) where Resource == View.ResourceViewModel {
+        self.resourceView = resourceView
+        self.loadingView = loadingView
+        self.errorView = errorView
+        self.mapper = { $0 }
+    }
 
     public func didStartLoading() {
         errorView.display(.init(message: .none))
