583cd4d74d2c604960ca3dbd89e97f05892995e8
tzc1234 <peopli9997@gmail.com>
2023-06-05 18:19:55 +0100

    Show loading more indicator while loading more

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -26,7 +26,7 @@ extension ListViewController {
     }
     
     private func numberOfRows(inSection section: Int) -> Int {
-        tableView.numberOfSections == section ? 0 : tableView.numberOfRows(inSection: section)
+        section < tableView.numberOfSections ? tableView.numberOfRows(inSection: section) : 0
     }
     
     private func cell(row: Int, section: Int) -> UITableViewCell? {
@@ -116,11 +116,19 @@ extension ListViewController {
     }
     
     func simulateLoadMoreFeedAction() {
-        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
+        guard let view = loadMoreFeedCell() else { return }
         
         let delegate = tableView.delegate
         let index = IndexPath(row: 0, section: feedLoadMoreSection)
         delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    }
+    
+    var isShowingLoadMoreFeedIndicator: Bool {
+        loadMoreFeedCell()?.isLoading == true
+    }
+    
+    private func loadMoreFeedCell() -> LoadMoreCell? {
+        cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
     
     func renderedFeedImageData(at index: Int) -> Data? {
